d42817fec48a3e6f97a7c49574b677c3dfc323e2
brickhu <brickhu@126.com>
2021-09-27 23:46:24 +0800

    增加antd和国际化

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
 <template>
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <div
-    class="menu">
-    <li v-for="item in menus" v-bind:key="item">{{item.name}}</li>
-    </div>
-  <router-view />
-</template>
+  <a-layout>
+    <a-layout-header
+      :style="{ position: 'fixed', zIndex: 1, width: '100%' }">
+      <a-menu
+        theme="dark"
+        mode="horizontal"
+        v-model:selectedKeys="selectedKeys"
+        :style="{ lineHeight: '64px' }">
+        <a-menu-item v-for="item in menus" v-bind:key="item" >{{item.name}}</a-menu-item>
+      </a-menu>
+      
+    </a-layout-header>
+    <a-layout-content
+      :style="{ padding: '0 50px', marginTop: '64px' }">
+      <router-view />
+      </a-layout-content>
+    <a-layout-footer>{{$t(`title.home`)}}</a-layout-footer>
+  </a-layout>
+  <div @click="change('zh-cn')">中文</div>
+  <div @click="change('en-us')">English</div>
+  <!-- <a-page-header
+      style="border: 0px solid rgb(235, 237, 240)"
+      title="enable"
+      sub-title="This is a subtitle"
+      @back="() => null"
+    /> -->
 
+  
+</template>
 <script>
+import { getCurrentInstance } from "vue";
+// import { useI18n } from 'vue-i18n'
+
 
 export default {
   name: 'App',
@@ -18,10 +42,14 @@ export default {
   },
   created(){
     console.log(this.$router.getRoutes())
+  },
+  setup(){
+    const { proxy } = getCurrentInstance()
+    function change(type){
+      proxy.$i18n.locale = type
+    }
+    return { change }
   }
-  // components: {
-  //   HelloWorld
-  // }
 }
 </script>
 
@@ -32,6 +60,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,21 @@ import App from './App.vue'
 import home from './components/home.vue'
 import about from './components/about.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import Antd from 'ant-design-vue'
+import "ant-design-vue/dist/antd.css"
+import { createI18n } from 'vue-i18n' 
+import en from './common/lang/en-us'
+import zh from './common/lang/zh-cn'
+
+
+const i18n = createI18n({
+  locale: 'zh-cn',
+  messages: {
+    'en-us': en,
+    'zh-cn': zh
+  }
+})
+
 
 const router = createRouter({
   history: createWebHistory(),
@@ -12,6 +27,11 @@ const router = createRouter({
   ]
 })
 
-const app = createApp(App);
+const app = createApp(App,i18n)
+
+app.config.productionTip = false
 app.use(router)
+app.use(Antd)
+app.use(i18n)
+
 app.mount('#app')
