3f5d8264ce04587b1c13d4f86e67c80812d873bc
brickhu <brickhu@126.com>
2021-09-27 15:31:40 +0800

    增加router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue' 
 import App from './App.vue'
+import home from './components/home.vue'
+import about from './components/about.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {path: '/', name:"home", component: home },
+    {path: '/about', name:"about", component: about}
+  ]
+})
+
+const app = createApp(App);
+app.use(router)
+app.mount('#app')
